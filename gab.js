8e282fd98f422487e24df1ba8ffc4063182926c3
Gabriel zanela machado de souza <101649513+Gabrielzanela@users.noreply.github.com>
2022-05-03 12:01:22 -0300

    Add files via upload

---
+++ gab.js
@@ -0,0 +1,17 @@
+
+numero = 1
+// while(numero != 3){
+//     numero = math.floor(math.random()* 10)
+//     console.log(numero)
+// }
+
+// do{
+// numero = math.floor(math.random()* 10)
+// console.log(numero != 3)
+// } while(numero != 3)
+
+numero = 1
+while(numero < 5000){
+    numero *= 4
+    console.log(numero)
+}
